e0f34328e9e89442b21e06c135af2a0dd93ec66e
Andrei Kandratovich <xandron.ak@gmail.com>
2025-01-27 00:07:33 +0100

    refactor(lib): moved commands to const

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,20 @@ pub mod commander;
 
 const UNDONE_LIST_COMMAND: &str = "undone";
 const FINISHED_LIST_COMMAND: &str = "finished";
+const SHOW_LIST_COMMAND: &str = "list";
+const ADD_TASK_COMMAND: &str = "add";
+const FINISH_TASK_COMMAND: &str = "finish";
+const CLEAR_TASKS_COMMAND: &str = "clear";
 
 pub fn run(mut args: Args) {
     let command = args.next();
 
     if let Some(command_text) = command {
         match command_text.as_str() {
-            "list" => commander::print_list(&mut args),
-            "add" => commander::add_task(&mut args),
-            "finish" => commander::mark_task_as_finished(&mut args),
-            "clear" => file_manager::clear_all_tasks(),
+            SHOW_LIST_COMMAND => commander::print_list(&mut args),
+            ADD_TASK_COMMAND => commander::add_task(&mut args),
+            FINISH_TASK_COMMAND => commander::mark_task_as_finished(&mut args),
+            CLEAR_TASKS_COMMAND => file_manager::clear_all_tasks(),
             _ => println!("Command {} was not found", command_text),
         }
     } else {
